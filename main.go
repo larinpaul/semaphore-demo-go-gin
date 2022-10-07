748484e670a58609d9ff8e2ccc69b2ffa6b2c62b
Pavel Larin <www.larinpavel@mail.ru>
2022-10-07 09:58:49 +0500

    initial commit

---
+++ main.go
@@ -0,0 +1,86 @@
+//// The third version of main.go
+package main
+
+import "github.com/gin-gonic/gin"
+
+var router *gin.Engine
+
+func main() {
+
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
+	// Handle Index
+	router.GET("/", showIndexPage)
+	// router.GET("/article/view/:article_id", getArticle)
+
+	router.Run()
+}
+
+// //// The second version of main.go
+// package main
+
+// import "github.com/gin-gonic/gin"
+
+// var router *gin.Engine
+
+// func main() {
+
+// 	// Set the router as the default one provided by Gin
+// 	router = gin.Default()
+
+// 	// Process the templates at the start so that they don't have to be loaded
+// 	// from the disk again. This makes serving HTML pages very fast.
+// 	router.LoadHTMLGlob("/templates/*")
+
+// 	// Handle Index
+// 	router.GET("/", showIndexPage)
+
+// 	// Start serving the application
+// 	router.Run()
+
+// }
+
+//// The first version of main.go:
+// package main
+
+// import (
+// 	"net/http"
+
+// 	"github.com/gin-gonic/gin"
+// )
+
+// var router *gin.Engine
+
+// func main() {
+
+// 	// Set the router as the default one provided by Gin
+// 	router = gin.Default()
+
+// 	// Process the templates at the start so that they don't have to be loaded
+// 	// from the disk again. This makes serving HTML pages very fast.
+// 	router.LoadHTMLGlob("templates/*")
+
+// 	// Define the route for the index page and display the index.html template
+// 	// To start with, we'll use an inline route handler. Later on, we'll create
+// 	// standalone functions that will be used as route handlers.
+// 	router.GET("/", func(c *gin.Context) {
+
+// 		// Call the HTML method of the Context to render a template
+// 		c.HTML(
+// 			// Set the HTTP status to 200 (OK)
+// 			http.StatusOK,
+// 			// Use the index.html template
+// 			"index.html",
+// 			// Pass the data that the page uses (in this case, 'title')
+// 			gin.H{
+// 				"title": "Home Page",
+// 			},
+// 		)
+
+// 	})
+
+// 	// Start serving the application
+// 	router.Run()
+
+// }
